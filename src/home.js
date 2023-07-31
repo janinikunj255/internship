5ea94cec3b18b5705dd86628237ebac2970f4eca
jani nikunj <njani5018@gmail.com>
2023-07-31 11:00:36 +0530

    react

---
+++ src/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function home() {
+  return (
+<>
+   <h1>HELLO EVERYONE</h1>
+   <img src='' />
+
+   </>
+  )
+}
---
+++ src/nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+export default function nav() {
+  return (
+ <ul>
+    <li>   <Link to="/">Home</Link>
+   </li>
+   <li> <Link to="/about">About</Link></li>
+ </ul>
+  )
+}
